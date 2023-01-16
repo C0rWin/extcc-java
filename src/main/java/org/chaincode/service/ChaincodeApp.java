fba2518ca3bdee9626ae1e61bfaebb5a0f5a2ffe
Artem Barger <artem@bargr.net>
2023-01-16 10:58:57 +0200

    Add and update .gitignore
    Signed-off-by: Artem Barger <artem@bargr.net>

--- src/main/java/org/chaincode/service/ChaincodeApp.java
+++ src/main/java/org/chaincode/service/ChaincodeApp.java
@@ -3,12 +3,11 @@ package org.chaincode.service;
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.ContractInterface;
 import org.hyperledger.fabric.contract.annotation.Contact;
+import org.hyperledger.fabric.contract.annotation.Default;
 import org.hyperledger.fabric.contract.annotation.Transaction;
 
-/**
- * Hello world!
- */
 @Contact
+@Default
 public class ChaincodeApp implements ContractInterface {
 
     public ChaincodeApp() {
--- src/test/java/org/chaincode/service/AppTest.java
+++ src/test/java/org/chaincode/service/AppTest.java
@@ -1,20 +1,47 @@
 package org.chaincode.service;
 
-import static org.junit.Assert.assertTrue;
-
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.netty.handler.ssl.ClientAuth;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws IOException {
+        final File keyFile = new File("/Users/c0rwin/workspace/myasset-java/crypto/extcc.key");
+        if (!keyFile.exists()) {
+            new FileNotFoundException("key file was not found");
+        }
+        final File keyCertChainFile = new File("/Users/c0rwin/workspace/myasset-java/crypto/extcc.pem");
+        if (!keyCertChainFile.exists()) {
+            new FileNotFoundException("certificate file was not found");
+        }
+        SslContextBuilder sslContextBuilder = SslContextBuilder.forServer(keyCertChainFile, keyFile);
+
+        sslContextBuilder.clientAuth(ClientAuth.REQUIRE);
+        sslContextBuilder.trustManager(new File("/Users/c0rwin/workspace/myasset-java/crypto/CA/CA.pem"));
+
+
+        final byte[] ckb = Files.readAllBytes(Paths.get("/Users/c0rwin/workspace/myasset-java/crypto/extcc64.key"));
+        final byte[] ccb = Files.readAllBytes(Paths.get("/Users/c0rwin/workspace/myasset-java/crypto/extcc64.pem"));
+        System.out.println(new String(ccb));
+        GrpcSslContexts.forClient().trustManager(new File("/Users/c0rwin/workspace/myasset-java/crypto/CA/CA.pem"))
+                .keyManager(new ByteArrayInputStream(Base64.getMimeDecoder().decode(ccb)),
+                        new ByteArrayInputStream(Base64.getMimeDecoder().decode(ckb)))
+                .build();
     }
 }
